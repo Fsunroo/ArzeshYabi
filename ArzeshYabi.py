1e71ea4fd42654d3116ab83fb0e86727933014f4
Mohammad Farhadi <Fsunroo@gmail.com>
2020-10-31 14:45:41 -0700

    Add files via upload

---
+++ ArzeshYabi.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+import time
+url="http://saba.sums.ac.ir/"
+UserName="9612104264"
+PassWord="2050917899"
+Driver=webdriver.Chrome("D:\programming\PROJECT\-----SOURCE-----\86\chromedriver.exe")
+Driver.get(url)
+Driver.maximize_window()
+Driver.find_element_by_xpath('/html/body/div[4]/div[2]/div[1]/a[1]/span[2]').click()
+time.sleep(1.5)
+Driver.switch_to_frame(Driver.find_element_by_id("iframe_040101"))
+Driver.find_element_by_xpath('/html/body/form/div[3]/div/div[1]/div[1]/div[3]/div/input').send_keys(UserName)
+Driver.find_element_by_xpath('/html/body/form/div[3]/div/div[1]/div[1]/div[4]/div/input').send_keys(PassWord)
+Driver.find_element_by_xpath('/html/body/form/div[3]/div/div[1]/div[1]/div[6]/input').click()
+
+Driver.switch_to_frame(Driver.find_element_by_id("iframe_FLASH_URL_TAB_ID"))
+Driver.find_element_by_xpath('/html/body/form/div[5]/div/div/table/tbody/tr/td[7]/input').click()
+
+while True:
+	Driver.switch_to.default_content()
+	time.sleep(2)
+	iframe = Driver.find_elements_by_tag_name('iframe')
+	Driver.switch_to_frame(iframe[1])      #ListOstad
+	Driver.find_element_by_xpath('/html/body/form/div[5]/div/div/table/tbody/tr[1]/td[12]/input').click()
+	Driver.switch_to.default_content()
+	time.sleep(1)
+	iframe = Driver.find_elements_by_tag_name('iframe')
+	Driver.switch_to_frame(iframe[2])      #Nomre
+
+	def choose_12():
+		for i in range(2,12):
+			x_path = '/html/body/form/div[4]/div/div/table/tbody/tr['+str(i)+']/td[12]/input[9]'
+			Driver.find_element_by_xpath(x_path).click()
+	choose_12()
+
+	Driver.find_element_by_xpath('/html/body/form/div[3]/input').click()   #Sabt
